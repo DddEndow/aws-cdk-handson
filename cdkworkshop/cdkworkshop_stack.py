bae840c558dc2d32b655da40f75e89eaa9a9309c
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2020-10-12 11:19:53 +0900

    [feat]lambdaの定義を追加

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
-    core
+    core,
+    aws_lambda as _lambda,
 )
 
 
@@ -8,3 +9,10 @@ class CdkworkshopStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
